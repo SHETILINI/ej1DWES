52b73ca6c45c718d84648cbe993a902a0fb74365
Borja Ferrer <borjetaa27@gmail.com>
2021-10-07 22:39:08 +0200

    Terminado de añadir los numeros decimales

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 $salario = 2000; 
+$salario_formateado = number_format($salario, 2, ',', '.');
 $travel = 400;
+$travel_formateado = number_format($travel, 2, ',', '.');
 $ret = 200;
+$rentencion_formateado = number_format($ret, 2, ',', '.');
 $taxbase;
-printf ("<p>The monthly payment corresponding to the month of: the worker John Smith, is, in gross: %.2f €</p>",$salario);
-printf ("<p>Plus Scroll: %+.2f €</p>",$travel);
-printf ("<p>Discount for Social Security: -%.2f €</p>",$ret);
+print ("<p>The monthly payment corresponding to the month of: the worker John Smith, is, in gross: $salario_formateado €</p>");
+printf ("<p>Plus Scroll: + $travel_formateado € </p>");
+printf ("<p>Discount for Social Security: -$rentencion_formateado € </p>");
 
 
 function tax() {
     global $salario, $travel, $ret;
     $taxbase = ($salario + $travel)-$ret;
     $taxbase_formateada = number_format($taxbase, 2, ',', '.');
-    printf ("<p>Tax base: %.2f €</p>",$taxbase);
+    print ("<p>Tax base: $taxbase_formateada €</p>");
     return $GLOBALS['taxbase'] = $taxbase;
 
 }
